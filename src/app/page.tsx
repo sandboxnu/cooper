24ccc924d543a2275c20260f4c0b6e038b011613
banushi-a <altonbanushi@icloud.com>
2024-01-29 12:32:17 -0500

    Added Prisma Schema for DB + NextAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default async function Home() {
   return (
-    <h1>Cooper</h1>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-cooper-blue font-bold">cooper</h1>
+    </div>
   );
 }
