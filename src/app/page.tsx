a7521d2854246aaa2c6600e0d1db2d564b258163
banushi-a <altonbanushi@icloud.com>
2024-01-29 17:58:06 -0500

    Prettier Fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default async function Home() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-cooper-blue font-bold">cooper</h1>
-    </div>
-  );
+  return <h1>Cooper</h1>;
 }
