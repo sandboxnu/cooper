6afa9585e61617dba4d1adc1f047093214c78347
Alton Banushi <altonbanushi@icloud.com>
2024-01-30 17:45:53 -0500

    Added Prisma Schema for DB + NextAuth (#22)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors: {
+        "cooper-blue": "#000088",
+      },
     },
   },
   plugins: [],
