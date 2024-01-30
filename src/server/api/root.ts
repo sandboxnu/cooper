a4b794b01609a9a5f57052071ada2a1c22e92946
banushi-a <altonbanushi@icloud.com>
2024-01-30 18:57:05 -0500

    Configured Company Router in Root Router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,14 @@
 import { createTRPCRouter } from "~/server/api/trpc";
+import { companyRouter } from "./routers/company";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  companyRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
