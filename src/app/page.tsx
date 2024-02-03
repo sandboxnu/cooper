f74f3adc55bdfbac3a7dfafa29ff74756bbfa3e8
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-02-03 09:30:59 -0500

    feat(auth): create an example sign-in sign-out workflow
    Only a demo 🙏

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { getServerAuthSession } from "~/server/auth";
 
 export default async function Home() {
@@ -5,20 +6,46 @@ export default async function Home() {
 
   if (!session) {
     return (
-      <div className="flex h-screen flex-col items-center justify-center space-y-2">
+      <div className="flex h-screen flex-col items-center justify-center space-y-4">
         <p className="text-3xl font-semibold text-red-500">
           You are not signed in!
         </p>
-        <p>Click Here</p>
+        <a
+          href="/api/auth/signin/google"
+          type="button"
+          className="mb-2 me-2 inline-flex items-center rounded-lg bg-[#4285F4] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#4285F4]/90 focus:outline-none focus:ring-4 focus:ring-[#4285F4]/50 dark:focus:ring-[#4285F4]/55"
+        >
+          <svg
+            className="me-2 h-4 w-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 18 19"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          Sign in with Google
+        </a>
       </div>
     );
   }
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-2">
+    <div className="flex h-screen flex-col items-center justify-center space-y-4">
       <p className="text-3xl font-semibold text-emerald-500">
         Welcome, {session.user.name}!
       </p>
+      <a
+        href="/api/auth/signout"
+        type="button"
+        className="mb-2 me-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      >
+        Sign Out
+      </a>
     </div>
   );
 }
