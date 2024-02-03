52cfd965c90e3c79f763e9e11b6e08c2e70ea645
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-02-03 08:53:28 -0500

    feat(auth): add next-auth authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { authOptions, getServerAuthSession } from "~/server/auth";
+
 export default async function Home() {
-  return <h1>Cooper</h1>;
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center space-y-2">
+        <p className="text-3xl font-semibold text-red-500">
+          You are not signed in!
+        </p>
+        <p>Click Here</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center space-y-2">
+      <p className="text-3xl font-semibold text-emerald-500">
+        Welcome, {session.user.name}!
+      </p>
+    </div>
+  );
 }
