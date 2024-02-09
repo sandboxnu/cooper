bc5d0da6294f3d5d6699983dff000c49a56cec6b
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-02-09 08:43:24 -0500

    feat(form): add a simple form field

---
+++ src/app/(protected)/review/page.tsx
@@ -0,0 +1,9 @@
+import { ProfileForm } from "~/components/review-form";
+
+export default async function Page() {
+  return (
+    <div className="container flex h-screen items-center justify-center">
+      <ProfileForm />
+    </div>
+  );
+}
---
+++ src/components/review-form.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+const formSchema = z.object({
+  reviewHeadline: z.string().min(8, {
+    message: "The review headline must be at least 8 characters.",
+  }),
+});
+
+export function ProfileForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      reviewHeadline: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="reviewHeadline"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Review Headline</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
