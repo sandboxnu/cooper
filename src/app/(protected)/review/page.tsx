181d69a6f59be98ea06fd641fffadbaf44b1617d
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-02-11 15:10:37 -0500

    feat(auth): protect the /review route

--- src/app/(protected)/review/page.tsx
+++ src/app/(protected)/review/page.tsx
@@ -1,6 +1,15 @@
 import { ReviewForm } from "~/components/review-form";
+import { getServerSession } from "next-auth";
+import { authOptions } from "~/server/auth";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
   return (
     <div className="container flex h-screen items-center justify-center">
       <ReviewForm />
