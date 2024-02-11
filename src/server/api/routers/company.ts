06236185a4aed926802d2de041fc93d5b3915f20
Alton Banushi <altonbanushi@icloud.com>
2024-02-11 15:43:16 -0500

    fix: fix company router errors when fetching companies that don't exist (#35)

--- src/server/api/routers/company.ts
+++ src/server/api/routers/company.ts
@@ -1,4 +1,3 @@
-import { z } from "zod";
 import {
   createTRPCRouter,
   protectedProcedure,
@@ -6,18 +5,31 @@ import {
 } from "~/server/api/trpc";
 import { createCompanySchema, updateCompanySchema } from "~/schema/company";
 import { getByIdSchema, getByNameSchema } from "~/schema/misc";
+import { TRPCError } from "@trpc/server";
 
 export const companyRouter = createTRPCRouter({
   list: publicProcedure.query(({ ctx }) => {
     return ctx.db.company.findMany();
   }),
-  getByName: publicProcedure.input(getByNameSchema).query(({ ctx, input }) => {
-    return ctx.db.company.findFirst({
-      where: {
-        name: input.name,
-      },
-    });
-  }),
+  getByName: publicProcedure
+    .input(getByNameSchema)
+    .query(async ({ ctx, input }) => {
+      // Ensure A Company Exists Before Getting It
+      const existingCompany = await ctx.db.company.findFirst({
+        where: {
+          name: input.name,
+        },
+      });
+
+      if (!existingCompany) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Company not found",
+        });
+      }
+
+      return existingCompany;
+    }),
   create: protectedProcedure
     .input(createCompanySchema)
     .mutation(({ ctx, input }) => {
@@ -32,7 +44,21 @@ export const companyRouter = createTRPCRouter({
     }),
   update: protectedProcedure
     .input(updateCompanySchema)
-    .mutation(({ ctx, input }) => {
+    .mutation(async ({ ctx, input }) => {
+      // Ensure A Company Exists Before Updating It
+      const existingCompany = await ctx.db.company.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingCompany) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Company not found",
+        });
+      }
+
       return ctx.db.company.update({
         where: {
           id: input.id,
@@ -42,11 +68,27 @@ export const companyRouter = createTRPCRouter({
         },
       });
     }),
-  delete: protectedProcedure.input(getByIdSchema).mutation(({ ctx, input }) => {
-    return ctx.db.company.delete({
-      where: {
-        id: input.id,
-      },
-    });
-  }),
+  delete: protectedProcedure
+    .input(getByIdSchema)
+    .mutation(async ({ ctx, input }) => {
+      // Ensure A Company Exists Before Deleting It
+      const existingCompany = await ctx.db.company.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+
+      if (!existingCompany) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Company not found",
+        });
+      }
+
+      return ctx.db.company.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
 });
