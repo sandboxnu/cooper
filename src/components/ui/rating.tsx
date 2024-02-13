01df4e0f5bcacb4cfaf534760b2cbc8544fa31ef
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-02-13 10:38:52 -0500

    feat(form): improve star rating ux

--- src/components/ui/rating.tsx
+++ src/components/ui/rating.tsx
@@ -1,15 +1,15 @@
 import { FieldPath, useFormContext } from "react-hook-form";
 import { ReviewFormType } from "~/components/review-form";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import { cn } from "~/lib/utils";
 
 type RatingProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-// See: https://github.com/shadcn-ui/ui/issues/1107#issuecomment-1918229523
 export const Rating = forwardRef<HTMLInputElement, RatingProps>(
   (props, ref) => {
     const name = props.name as FieldPath<ReviewFormType>;
     const { register, setValue, getValues } = useFormContext<ReviewFormType>();
+    const [hoveredIndex, setHoveredIndex] = useState<number>(0);
 
     return (
       <div className="flex">
@@ -21,18 +21,26 @@ export const Rating = forwardRef<HTMLInputElement, RatingProps>(
               key={i}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              strokeWidth={1}
+              strokeWidth={0.75}
               stroke="currentColor"
               className={cn(
-                "size-10",
-                i < +getValues(name)
+                "size-14",
+                i < hoveredIndex || i < +getValues(name)
                   ? "fill-primary"
                   : "fill-primary-foreground",
-                "hover:cursor-pointer",
+                "pr-2 hover:cursor-pointer",
               )}
-              onClick={() =>
-                setValue(name, i === +getValues(name) - 1 ? 0 : i + 1)
-              }
+              onMouseEnter={() => setHoveredIndex(i + 1)}
+              onMouseLeave={() => setHoveredIndex(0)}
+              onClick={() => {
+                const currentValue = +getValues(name);
+                if (currentValue === i + 1) {
+                  setValue(name, 0);
+                  setHoveredIndex(0);
+                } else {
+                  setValue(name, i + 1);
+                }
+              }}
             >
               <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
             </svg>
