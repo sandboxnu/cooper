70f6b206bcc2a0412e69c7882004df71f7cb2039
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-03-01 16:46:13 -0500

    feat(form): add company id as a search param

--- src/app/(protected)/review/page.tsx
+++ src/app/(protected)/review/page.tsx
@@ -1,13 +1,37 @@
 import { ReviewForm } from "~/components/review-form";
 import { getServerSession } from "next-auth";
 import { authOptions } from "~/server/auth";
-import { redirect } from "next/navigation";
+import { redirect, notFound } from "next/navigation";
+import { api } from "~/trpc/server";
+import { TRPCClientError } from "@trpc/client";
 
-export default async function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    id?: string;
+  };
+}) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/");
+  }
+
+  if (!searchParams?.id) {
+    notFound();
+  }
+
+  let company;
+
+  try {
+    company = await api.company.getById.query({
+      id: searchParams.id,
+    });
+  } catch (error) {
+    if (error instanceof TRPCClientError) {
+      return notFound();
+    }
   }
 
   return (
