43ca0e1d526d3396247be65fdb433e942b4a0dd4
banushi-a <altonbanushi@icloud.com>
2024-03-25 12:59:03 -0400

    fix: removed async from exports (not needed)

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -3,11 +3,11 @@
 import { Button } from "~/components/ui/button";
 import { signIn } from "next-auth/react";
 
-export default async function LoginButton() {
+export default function LoginButton() {
   return (
     <Button
       type="button"
-      className="rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      className="rounded-lg bg-cooper-blue-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-cooper-blue-200 hover:text-cooper-blue-600 focus:outline-none focus:ring-4 focus:ring-white"
       onClick={() => signIn("google")}
     >
       Sign In
--- src/components/logout-button.tsx
+++ src/components/logout-button.tsx
@@ -3,14 +3,14 @@
 import { Button } from "~/components/ui/button";
 import { signOut } from "next-auth/react";
 
-export default async function LogoutButton() {
+export default function LogoutButton() {
   return (
     <Button
       onClick={() => {
         signOut({ callbackUrl: "/" });
       }}
       type="button"
-      className="rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+      className="rounded-lg bg-cooper-blue-400 px-5 py-2.5 text-sm font-medium text-white hover:bg-cooper-blue-200 hover:text-cooper-blue-600 focus:outline-none focus:ring-4 focus:ring-white"
     >
       Sign Out
     </Button>
