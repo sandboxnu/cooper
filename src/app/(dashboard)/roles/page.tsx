b7c7084bf9fd2f65fb60588aaa8c34dad3d8d914
banushi-a <altonbanushi@icloud.com>
2024-04-14 10:54:05 -0400

    fix: resolve incorrect merge

--- src/app/(dashboard)/roles/page.tsx
+++ src/app/(dashboard)/roles/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Review } from "@prisma/client";
 import { useState } from "react";
 import { ReviewCard } from "~/components/review-card";
 import { ReviewCardPreview } from "~/components/review-card-preview";
@@ -10,7 +11,9 @@ import { api } from "~/trpc/react";
 export default function Roles() {
   const reviews = api.review.list.useQuery();
 
-  const [selectedReviewIdx, setSelectedReviewIdx] = useState<number>(0);
+  const [selectedReview, setSelectedReview] = useState<Review | undefined>(
+    reviews.data ? reviews.data[0] : undefined,
+  );
 
   return (
     <>
@@ -19,9 +22,9 @@ export default function Roles() {
       {reviews.data && (
         <div className="mb-8 grid w-4/5 grid-cols-5 gap-4 lg:w-3/4">
           <div className="col-span-2 gap-3">
-            {reviews.data.map((review, idx) => {
+            {reviews.data.map((review) => {
               return (
-                <div key={review.id} onClick={() => setSelectedReviewIdx(idx)}>
+                <div key={review.id} onClick={() => setSelectedReview(review)}>
                   <ReviewCardPreview
                     reviewObj={review}
                     className={cn("mb-4 hover:border-2")}
@@ -32,9 +35,7 @@ export default function Roles() {
           </div>
           <div className="col-span-3">
             {reviews.data.length > 0 && reviews.data[0] && (
-              <ReviewCard
-                reviewObj={reviews.data[selectedReviewIdx] || reviews.data[0]}
-              />
+              <ReviewCard reviewObj={selectedReview || reviews.data[0]} />
             )}
           </div>
         </div>
