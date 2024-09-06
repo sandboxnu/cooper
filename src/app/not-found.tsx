d6126949d5822f70ad2e5580510f07e9f3190b49
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-09-06 17:38:21 -0400

    chore: remove everything

--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-semibold">404 - Page Not Found</h1>
-    </div>
-  );
-}
