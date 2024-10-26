c0772eca8323ceecb3f5d5696019b61a2cc1974d
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-10-26 19:45:56 -0400

    feat: add onboarding dialog skeleton

--- apps/web/src/app/(pages)/(dashboard)/page.tsx
+++ apps/web/src/app/(pages)/(dashboard)/page.tsx
@@ -1,10 +1,16 @@
+import { auth } from "@cooper/auth";
+
+import { OnboardingDialog } from "~/app/_components/onboarding/dialog";
 import SearchFilter from "~/app/_components/search/search-filter";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div className="flex h-[85vh] flex-col">
       <div className="flex h-full flex-col items-center justify-center">
         <SearchFilter />
+        <OnboardingDialog isOpen session={session} />
       </div>
     </div>
   );
---
+++ apps/web/src/app/_components/onboarding/dialog.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+import type { Session } from "@cooper/auth";
+import { Button } from "@cooper/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@cooper/ui/dialog";
+
+import { api } from "~/trpc/react";
+
+interface OnboardingDialogProps {
+  isOpen: boolean;
+  session: Session | null;
+}
+
+export function OnboardingDialog({ isOpen, session }: OnboardingDialogProps) {
+  const [open, setOpen] = useState<boolean>(isOpen);
+
+  const profile = api.profile.getCurrentUser.useQuery();
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-center text-xl font-bold text-cooper-blue-600">
+            {session ? "Create a Cooper Account" : "Login"}
+          </DialogTitle>
+        </DialogHeader>
+        {!session && <SignInWithGoogleButton />}
+        {!profile.data && <p className="text-center">User Onboarding Flow</p>}
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+const SignInWithGoogleButton = () => {
+  const handleGoogleSignIn = () => {
+    void signIn("google");
+  };
+
+  return (
+    <div className="mt-6 flex justify-center">
+      <Button
+        onClick={handleGoogleSignIn}
+        className="flex items-center gap-2 border-none bg-white text-gray-700 hover:bg-gray-100"
+      >
+        <svg
+          className="h-5 w-5"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 488 512"
+        >
+          <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+        </svg>
+        Sign in with Google
+      </Button>
+    </div>
+  );
+};
