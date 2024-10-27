2afc5e04521eec426e6805f6e6e17d369b18ea11
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-10-26 22:13:16 -0400

    chore: format

--- apps/web/src/app/_components/themed/onboarding/input.tsx
+++ apps/web/src/app/_components/themed/onboarding/input.tsx
@@ -3,7 +3,7 @@ import { Input as InputPrimitive } from "@cooper/ui/input";
 export function Input(props: React.ComponentProps<typeof InputPrimitive>) {
   return (
     <InputPrimitive
-      className="border-cooper-gray-300 text-cooper-gray-400 h-12 rounded-full border-2 text-lg shadow-none"
+      className="h-12 rounded-full border-2 border-cooper-gray-300 text-lg text-cooper-gray-400 shadow-none"
       {...props}
     />
   );
--- apps/web/src/app/_components/themed/onboarding/select.tsx
+++ apps/web/src/app/_components/themed/onboarding/select.tsx
@@ -18,7 +18,7 @@ export const Select: React.FC<SelectProps> = ({
     <div className="relative w-full">
       <select
         className={cn(
-          "border-cooper-gray-300 text-cooper-gray-400 h-12 w-[275px] appearance-none rounded-full border-2 bg-transparent px-4 pr-10 text-lg shadow-none",
+          "h-12 w-[275px] appearance-none rounded-full border-2 border-cooper-gray-300 bg-transparent px-4 pr-10 text-lg text-cooper-gray-400 shadow-none",
           className,
         )}
         {...props}
@@ -30,7 +30,7 @@ export const Select: React.FC<SelectProps> = ({
           </option>
         ))}
       </select>
-      <div className="text-cooper-gray-400 pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-cooper-gray-400">
         <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
           <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" />
         </svg>
