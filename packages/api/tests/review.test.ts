a68bc7f639b46c08ca83dc54b42d4fb773dcaee7
gpalmer27 <palmer.gi@northeastern.edu>
2024-10-31 21:16:26 -0400

    started testing

---
+++ packages/api/tests/review.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+//import {  } from './api/src'
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(1+2).toBe(3)
+})
+
+// test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3)
+//   })
