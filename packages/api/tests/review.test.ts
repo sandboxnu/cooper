580d7f4d3b14082b3dc17dfe2e818b8fe2b4aedb
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-11-10 15:16:08 -0500

    chore: update test description

--- packages/api/tests/review.test.ts
+++ packages/api/tests/review.test.ts
@@ -1,4 +1,4 @@
-import { afterAll, beforeEach, describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 
 import { Session } from "@cooper/auth";
 
