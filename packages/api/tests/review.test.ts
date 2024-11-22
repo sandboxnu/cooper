1491df5eca679c7115d4f37382eff559a2fdcf12
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-11-22 10:45:35 -0500

    chore: add mocks

--- packages/api/tests/review.test.ts
+++ packages/api/tests/review.test.ts
@@ -1,4 +1,18 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+
+vi.mock("@cooper/db/client", () => ({
+  db: {
+    query: {
+      Review: {
+        findMany: vi.fn(),
+      },
+    },
+  },
+}));
+
+vi.mock("@cooper/auth", () => ({
+  auth: vi.fn(),
+}));
 
 describe("Review Router", () => {
   test("this should pass", () => {
