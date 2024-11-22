0b743f4f31964d75e1e0ead330026acad7a37fb3
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-11-22 11:00:29 -0500

    chore: test passing locally fingers crossed

--- packages/api/tests/review.test.ts
+++ packages/api/tests/review.test.ts
@@ -1,4 +1,14 @@
-import { describe, expect, test, vi } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+
+import type { Session } from "@cooper/auth";
+import type { ReviewType } from "@cooper/db/schema";
+import { desc } from "@cooper/db";
+import { db } from "@cooper/db/client";
+import { Review } from "@cooper/db/schema";
+
+import { appRouter } from "../src/root";
+import { createCallerFactory, createTRPCContext } from "../src/trpc";
+import { data } from "./mocks/review";
 
 vi.mock("@cooper/db/client", () => ({
   db: {
@@ -14,8 +24,34 @@ vi.mock("@cooper/auth", () => ({
   auth: vi.fn(),
 }));
 
-describe("Review Router", () => {
-  test("this should pass", () => {
-    expect(true).toBe(true);
+describe("Review Router", async () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+    vi.mocked(db.query.Review.findMany).mockResolvedValue(data as ReviewType[]);
+  });
+
+  const session: Session = {
+    user: {
+      id: "1",
+    },
+    expires: "1",
+  };
+
+  const ctx = await createTRPCContext({
+    session,
+    headers: new Headers(),
+  });
+
+  const caller = createCallerFactory(appRouter)(ctx);
+
+  test("list endpoint returns all reviews", async () => {
+    const reviews = await caller.review.list({});
+
+    expect(reviews).toEqual(data);
+
+    expect(db.query.Review.findMany).toHaveBeenCalledWith({
+      orderBy: desc(Review.id),
+      where: undefined,
+    });
   });
 });
