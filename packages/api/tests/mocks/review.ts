e8a303d93f4ffdde672ce4b525d75773f9a29b2b
Rishikesh Kanabar <63854326+RishikeshNK@users.noreply.github.com>
2024-11-22 19:08:11 -0500

    Cooper Testing is CURSED (#94)
    * chore: add a mock test
    
    * chore: fix web app linting
    
    * chore: add mock and turbo
    
    * chore: add mocks
    
    * chore: test passing locally fingers crossed
    
    * chore: disable check
    
    * chore: ignore unbound methods
    
    * chore: add another test
    
    * chore: add two more tests
    
    * chore: remove unused import

---
+++ packages/api/tests/mocks/review.ts
@@ -0,0 +1,5 @@
+export const data = [
+  { id: "1", workTerm: "SPRING", workEnvironment: "REMOTE" },
+  { id: "2", workTerm: "FALL", workEnvironment: "INPERSON" },
+  { id: "3", workTerm: "SUMMER", workEnvironment: "HYBRID" },
+];
