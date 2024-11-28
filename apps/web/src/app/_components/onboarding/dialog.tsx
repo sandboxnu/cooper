24ee7127cdbdf737bc7e66e2a63422ac228eee34
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2024-11-28 15:49:22 -0500

    refactor: rename variables and add dialog title

--- apps/web/src/app/_components/onboarding/dialog.tsx
+++ apps/web/src/app/_components/onboarding/dialog.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 
 import type { Session } from "@cooper/auth";
 import { Button } from "@cooper/ui/button";
-import { Dialog, DialogContent } from "@cooper/ui/dialog";
+import { Dialog, DialogContent, DialogTitle } from "@cooper/ui/dialog";
 
 import { OnboardingForm } from "~/app/_components/onboarding/onboarding-form";
 import { api } from "~/trpc/react";
@@ -48,7 +48,7 @@ export function OnboardingDialog({
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="p-12 sm:max-w-[720px]">
-        {shouldShowSignIn && <SignInWithGoogleButton />}
+        {shouldShowSignIn && <SignInWithGooglePrompt />}
         {shouldShowOnboarding && (
           <OnboardingForm userId={session.user.id} closeDialog={closeDialog} />
         )}
@@ -57,27 +57,32 @@ export function OnboardingDialog({
   );
 }
 
-const SignInWithGoogleButton = () => {
+const SignInWithGooglePrompt = () => {
   const handleGoogleSignIn = () => {
     void signIn("google");
   };
 
   return (
-    <div className="mt-6 flex justify-center">
-      <Button
-        onClick={handleGoogleSignIn}
-        className="flex items-center gap-2 border-none bg-white text-gray-700 hover:bg-gray-100"
-      >
-        <svg
-          className="h-5 w-5"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 488 512"
+    <>
+      <DialogTitle className="pb-4 text-center text-2xl font-bold text-cooper-blue-600">
+        Login
+      </DialogTitle>
+      <div className="mt-6 flex justify-center">
+        <Button
+          onClick={handleGoogleSignIn}
+          className="flex items-center gap-2 border-none bg-white text-gray-700 hover:bg-gray-100"
         >
-          <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
-        </svg>
-        Sign in with Google
-      </Button>
-    </div>
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 488 512"
+          >
+            <path d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z" />
+          </svg>
+          Sign in with Google
+        </Button>
+      </div>
+    </>
   );
 };
