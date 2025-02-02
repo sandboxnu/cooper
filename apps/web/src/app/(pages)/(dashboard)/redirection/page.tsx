a8d4a8913741f8d542f8ad5d9758d656017b9841
Gillian Palmer <147459191+gpalmer27@users.noreply.github.com>
2025-02-02 16:05:04 -0500

    Husky Login Requirement (#115)
    * tried to add check
    
    * added redirection page
    
    * fixed linting
    
    * fixed linting
    
    * fixed linting
    
    * updated documentation
    
    * fixed minor comments
    
    * formatting
    
    ---------
    
    Co-authored-by: Alton Banushi <altonbanushi@icloud.com>

---
+++ apps/web/src/app/(pages)/(dashboard)/redirection/page.tsx
@@ -0,0 +1,15 @@
+export default function ErrorPage() {
+  return (
+    <div className="flex h-[85vh] flex-col">
+      <div className="flex h-full items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold text-gray-900">
+            Authentication Error
+          </h1>
+          <p className="text-gray-600">You must log in with husky.neu.edu</p>
+          <p className="text-gray-600">Click the sign in button to try again</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- apps/web/src/app/_components/auth/login-button.tsx
+++ apps/web/src/app/_components/auth/login-button.tsx
@@ -8,7 +8,7 @@ export default function LoginButton() {
         className="rounded-xl border-cooper-blue-400 bg-cooper-blue-400 px-5 py-2.5 text-sm font-semibold text-white hover:border-cooper-blue-200 hover:bg-cooper-blue-200 hover:text-cooper-blue-600 focus:outline-none focus:ring-4 focus:ring-white"
         formAction={async () => {
           "use server";
-          await signIn("google");
+          await signIn("google", { redirectTo: "/" });
         }}
       >
         Sign In
