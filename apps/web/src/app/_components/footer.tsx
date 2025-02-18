10482f9b5beae1803c3d5219c87b24dd781a8cce
mattrwang <wang.mat@northeastern.edu>
2025-02-17 22:23:52 -0500

    added form link area to footer

--- apps/web/src/app/_components/footer.tsx
+++ apps/web/src/app/_components/footer.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
+import { MessageSquare } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="flex h-20 w-full grid-cols-2 items-center justify-between border-t border-t-[#919191] bg-white px-4">
+    <footer className="flex h-20 w-full grid-cols-2 flex-col items-center justify-between border-t border-t-[#919191] bg-white px-4 md:flex-row">
       <Link
         href="https://github.com/sandboxnu/cooper"
         target="_blank"
@@ -18,9 +19,17 @@ export default function Footer() {
         />
         <h2 className="text-xl font-semibold">Checkout cooper on GitHub</h2>
       </Link>
-      <a href="https://clearbit.com" target="_blank" className="italic">
-        Logos provided by Clearbit
-      </a>
+      <div className="flex flex-col items-center md:flex-row md:gap-6">
+        <Link href="____" target="_blank" className="flex items-center gap-2">
+          <MessageSquare size={20} />
+          <h2 className="text-lg font-semibold">
+            Submit Feedback or Bug Reports!
+          </h2>
+        </Link>
+        <a href="https://clearbit.com" target="_blank" className="italic">
+          Logos provided by Clearbit
+        </a>
+      </div>
     </footer>
   );
 }
