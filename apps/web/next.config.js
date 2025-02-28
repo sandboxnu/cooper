fb07fc9020da349c202177220c474804a2e8f56b
Alton Banushi <altonbanushi@icloud.com>
2025-02-28 18:12:22 -0500

    Start Updating Styles / New Header (Not Mobile Yet) with Michael (#145)
    * Start Updating Styles / New Header (Not Mobile Yet) with Michael
    
    * Overflow Issue
    
    * Padding issue
    
    * Fix header styles

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -30,6 +30,12 @@ const config = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
