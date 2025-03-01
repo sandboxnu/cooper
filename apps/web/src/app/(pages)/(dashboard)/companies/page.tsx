7db31e157ee574c4cda023be24604e058bec32a7
Xiaole Su <suxiaole0223@gmail.com>
2025-02-28 19:51:29 -0500

    fix: no longer async company page // feat: new company cards

--- apps/web/src/app/(pages)/(dashboard)/companies/page.tsx
+++ apps/web/src/app/(pages)/(dashboard)/companies/page.tsx
@@ -1,35 +1,38 @@
-import { unstable_noStore as noStore } from "next/cache";
+"use client";
 
+import LoadingResults from "~/app/_components/loading-results";
 import NoResults from "~/app/_components/no-results";
-import { RoleReviewCard } from "~/app/_components/reviews/role-review-card";
-import { api } from "~/trpc/server";
+import { CompanyCardPreview } from "~/app/_components/reviews/company-card-preview";
+import { api } from "~/trpc/react";
 
-export default async function Companies() {
-  /**
-   * FIXME: This is a temporary fix, figure out how to get build command working without noStore();
-   * @returns A promise containing the roles from the database
-   */
-  async function getRoles() {
-    noStore();
-    const roles = await api.role.list();
-    return roles;
-  }
+export default function Companies() {
+  // /**
+  //  * FIXME: This is a temporary fix, figure out how to get build command working without noStore();
+  //  * @returns A promise containing the roles from the database
+  //  */
+  // async function getRoles() {
+  //   noStore();
+  //   const roles = await api.role.list();
+  //   return roles;
+  // }
 
-  const roles = await getRoles();
+  const companies = api.company.list.useQuery();
 
   return (
     <>
-      {roles.length > 0 ? (
-        <div className="grid grid-cols-1 gap-4 overflow-y-scroll md:grid-cols-2 xl:grid-cols-3">
-          {roles.map((role) => {
-            return (
-              <RoleReviewCard key={role.id} roleObj={role} className="mb-4" />
-            );
-          })}
+      {companies.isSuccess && companies.data.length > 0 ? (
+        <div className="mb-8 grid h-[70dvh] w-3/4 grid-cols-1 gap-4 overflow-y-scroll md:grid-cols-2 xl:grid-cols-3">
+          {companies.data.map((company) => (
+            <div key={company.id} className="cursor-pointer">
+              <CompanyCardPreview companyObj={company} className="mb-4" />
+            </div>
+          ))}
         </div>
-      ) : (
+      ) : companies.isSuccess && companies.data.length === 0 ? (
         <NoResults />
-      )}
+      ) : companies.isPending ? (
+        <LoadingResults />
+      ) : null}
     </>
   );
 }
---
+++ apps/web/src/app/_components/reviews/company-card-preview.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+
+import type { CompanyType } from "@cooper/db/schema";
+import { cn } from "@cooper/ui";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@cooper/ui/card";
+
+interface CompanyCardPreviewProps {
+  className?: string;
+  companyObj: CompanyType;
+}
+
+export function CompanyCardPreview({
+  className,
+  companyObj,
+}: CompanyCardPreviewProps) {
+  return (
+    <Card
+      className={cn(
+        "flex h-[26rem] w-[100%] flex-col justify-between overflow-hidden rounded-3xl",
+        className,
+      )}
+    >
+      <div>
+        <CardHeader className="pb-3">
+          <div className="flex items-center justify-start space-x-4">
+            <Image
+              src={`https://logo.clearbit.com/${companyObj.name.replace(/\s/g, "")}.com`}
+              width={75}
+              height={75}
+              alt={`Logo of ${companyObj.name}`}
+              className="rounded-2xl border"
+            />
+            <div>
+              <CardTitle className="text-xl">{companyObj.name}</CardTitle>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="grid">
+          <div className="m-4 flex items-center space-x-8">
+            <div className="flex flex-col text-sm">
+              <h4 className="font-semibold">Industry</h4>
+              <p>{companyObj.industry}</p>
+            </div>
+            <div className="flex flex-col text-sm">
+              <h4 className="font-semibold">DNE</h4>
+              <p>{companyObj.location}</p>
+            </div>
+          </div>
+          <div className="m-4 flex items-center space-x-4">
+            <p className="text-sm">{companyObj.description}</p>
+          </div>
+        </CardContent>
+      </div>
+      <CardFooter className="items-end justify-end text-xs">
+        {(companyObj.totalReviews ?? 0) + " Reviews"}
+      </CardFooter>
+    </Card>
+  );
+}
