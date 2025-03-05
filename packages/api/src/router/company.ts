0b731aaff48a8ea3242ec96fd8aaf9a991c64815
gpalmer27 <palmer.gi@northeastern.edu>
2025-03-05 10:18:34 -0500

    added endpoint for companies

--- packages/api/src/router/company.ts
+++ packages/api/src/router/company.ts
@@ -2,7 +2,7 @@ import type { TRPCRouterRecord } from "@trpc/server";
 import { z } from "zod";
 
 import { desc, eq } from "@cooper/db";
-import { Company, CreateCompanySchema } from "@cooper/db/schema";
+import { Company, Review, CreateCompanySchema } from "@cooper/db/schema";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
@@ -38,4 +38,50 @@ export const companyRouter = {
   delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
     return ctx.db.delete(Company).where(eq(Company.id, input));
   }),
+
+  getAverageById: publicProcedure
+  .input(z.object({ companyId: z.string() }))
+  .query(async ({ ctx, input }) => {
+
+    const reviews = ctx.db.query.Review.findMany({
+      where: eq(Review.companyId, input.companyId),
+    })
+
+    const totalReviews = (await reviews).length;
+
+    const averageOverallRating =
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + review.overallRating, 0) / totalReviews
+        : 0; 
+    const averageHourlyPay = 
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + Number(review.hourlyPay), 0) / totalReviews
+        : 0; 
+    const averageInterviewDifficulty = 
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + review.interviewDifficulty, 0) / totalReviews
+        : 0; 
+    const averageCultureRating = 
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + review.cultureRating, 0) / totalReviews
+        : 0; 
+    const averageSupervisorRating = 
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + review.supervisorRating, 0) / totalReviews
+        : 0; 
+    const averageInterviewRating = 
+      totalReviews > 0
+        ? (await reviews).reduce((sum, review) => sum + review.interviewRating, 0) / totalReviews
+        : 0; 
+
+    return {
+      averageOverallRating: averageOverallRating, 
+      averageHourlyPay: averageHourlyPay,
+      averageInterviewDifficulty: averageInterviewDifficulty,
+      averageCultureRating: averageCultureRating,
+      averageSupervisorRating: averageSupervisorRating,
+      averageInterviewRating: averageInterviewRating
+    }
+  }),
+
 } satisfies TRPCRouterRecord;
