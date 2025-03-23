2b71bfcbeb404aea86a9b1fe2f56cd1a82f21fbd
Xiaole Su <suxiaole0223@gmail.com>
2025-03-22 20:39:07 -0400

    fix for linting

--- apps/web/src/app/(pages)/(dashboard)/(roles)/role/page.tsx
+++ apps/web/src/app/(pages)/(dashboard)/(roles)/role/page.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 import { RoleType } from "@cooper/db/schema";
 
-import RenderAllRoles from "~/app/_components/companies/all-company-roles";
-import { CompanyAbout } from "~/app/_components/companies/company-about";
-import { CompanyReview } from "~/app/_components/companies/company-reviews";
 import LoadingResults from "~/app/_components/loading-results";
 import NoResults from "~/app/_components/no-results";
 import { RoleInfo } from "~/app/_components/reviews/role-info";
 import { api } from "~/trpc/react";
-import { prettyIndustry } from "~/utils/stringHelpers";
 
 export default function Role() {
   const searchParams = useSearchParams();
