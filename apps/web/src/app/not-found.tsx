440fa9575b4a5890f96a95e8dfe8a12e7a18bc60
Michael Song <songmichael11@gmail.com>
2025-03-27 18:47:08 -0400

    update error page

--- apps/web/src/app/not-found.tsx
+++ apps/web/src/app/not-found.tsx
@@ -1,7 +1,22 @@
+import HeaderLayout from "~/app/_components/header-layout";
+
 export default function NotFound() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-semibold">404 - Page Not Found</h1>
-    </div>
+    <HeaderLayout>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="text-4xl font-semibold">404 - Page Not Found</h1>
+        <Image
+          src="/svg/hidingLogo.svg"
+          alt="Co-op Review Logo"
+          width={400}
+          height={100}
+          className="max-w-full xl:hidden"
+        />
+        <h1 className="text-4xl font-semibold">404 - Page Not Found</h1>
+      </div>
+    </HeaderLayout>
   );
 }
+//router.back for back button
+//client component
+//make button a server component
