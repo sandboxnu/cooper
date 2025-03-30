88a592797c88984aca9d774ce383598f266b7f2b
tracyyh <125928988+tracyyh@users.noreply.github.com>
2025-03-30 15:12:34 -0400

    Role Request Frontend (#162)
    * new role request frontend
    
    * form yippee
    
    * update reload time
    
    * address PR comments
    
    * fix form validation
    
    * remove comment
    
    * reduce calls
    
    * remove console log and auth
    
    * formatting
    
    * prettier
    
    * add minimum requirement on role title
    
    ---------
    
    Co-authored-by: Alton Banushi <altonbanushi@icloud.com>

--- apps/web/src/app/_components/companies/all-company-roles.tsx
+++ apps/web/src/app/_components/companies/all-company-roles.tsx
@@ -4,7 +4,7 @@ import { cn } from "@cooper/ui";
 
 import { api } from "~/trpc/react";
 import LoadingResults from "../loading-results";
-import NoResults from "../no-results";
+import NewRoleCard from "../reviews/new-role-card";
 import { RoleCardPreview } from "../reviews/role-card-preview";
 
 interface RenderAllRolesProps {
@@ -18,28 +18,35 @@ export default function RenderAllRoles({ company }: RenderAllRolesProps) {
   return (
     <>
       <h2 className="mb-4 text-2xl">Job Postings</h2>
-      {roles.isSuccess && roles.data.length > 0 ? (
-        <div className="mb-8 grid h-[30dvh] w-full grid-cols-1 gap-3 p-1 md:grid-cols-2 xl:grid-cols-3">
-          {roles.data.map((role) => {
-            return (
-              <div
-                key={role.id}
-                className="p-2"
-                onClick={() => router.push(`/role?id=${role.id}`)}
-              >
-                <RoleCardPreview
-                  reviewObj={role}
-                  className={cn("mb-4 hover:bg-cooper-gray-100")}
-                />
-              </div>
-            );
-          })}
-        </div>
-      ) : roles.isSuccess && roles.data.length === 0 ? (
-        <NoResults />
-      ) : roles.isPending ? (
+      {roles.isPending ? (
         <LoadingResults />
-      ) : null}
+      ) : (
+        <div className="mb-8 grid h-[30dvh] w-full grid-cols-1 gap-3 p-1 md:grid-cols-2 xl:grid-cols-3">
+          {roles.isSuccess && roles.data.length > 0 && (
+            <>
+              {roles.data.map((role) => {
+                return (
+                  <div
+                    key={role.id}
+                    className="p-2"
+                    onClick={() => router.push(`/role?id=${role.id}`)}
+                  >
+                    <RoleCardPreview
+                      reviewObj={role}
+                      className={cn("mb-4 hover:bg-cooper-gray-100")}
+                    />
+                  </div>
+                );
+              })}
+            </>
+          )}
+          {company && (
+            <div className="p-2">
+              <NewRoleCard companyId={company} />
+            </div>
+          )}
+        </div>
+      )}
     </>
   );
 }
