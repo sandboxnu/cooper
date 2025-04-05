1af9ca193eba65a891246d7f54ba2a143405867f
Rishikesh Kanabar <rishikesh.kanabar@gmail.com>
2025-04-05 15:17:31 -0400

    doesnt works yet smh

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,4 +1,14 @@
 import type { Config } from "drizzle-kit";
+import { neonConfig } from "@neondatabase/serverless";
+
+console.log("Loading Drizzle config...");
+
+if (!process.env.VERCEL_ENV) {
+  neonConfig.wsProxy = (host) => `${host}:5433/v1`;
+  neonConfig.useSecureWebSocket = false;
+  neonConfig.pipelineTLS = false;
+  neonConfig.pipelineConnect = false;
+}
 
 if (!process.env.POSTGRES_URL) {
   throw new Error("Missing POSTGRES_URL");
@@ -6,14 +16,19 @@ if (!process.env.POSTGRES_URL) {
 
 const nonPoolingUrl = process.env.POSTGRES_URL.replace(":6543", ":5432");
 
+console.log("Using non-pooling URL:", nonPoolingUrl);
+
 export default {
   schema: "./src/schema",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: nonPoolingUrl,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    ssl:
+      process.env.VERCEL_ENV === "production"
+        ? {
+            rejectUnauthorized: false,
+          }
+        : false,
   },
 } satisfies Config;
