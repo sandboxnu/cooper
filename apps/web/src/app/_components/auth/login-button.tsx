e25189517b8a77155f4a2df1efef595300c20263
banushi-a <altonbanushi@icloud.com>
2025-04-13 13:46:51 -0400

    profile button update

--- apps/web/src/app/_components/auth/login-button.tsx
+++ apps/web/src/app/_components/auth/login-button.tsx
@@ -1,18 +1,25 @@
+import Image from "next/image";
+
 import { signIn } from "@cooper/auth";
-import { Button } from "@cooper/ui/button";
 
 export default function LoginButton() {
   return (
     <form>
-      <Button
-        className="rounded-lg border-none bg-cooper-yellow-500 px-5 py-2.5 text-sm font-semibold text-white hover:bg-cooper-yellow-300 hover:text-white focus:outline-none focus:ring-4 focus:ring-white"
+      <button
         formAction={async () => {
           "use server";
           await signIn("google", { redirectTo: "/" });
         }}
+        className="pt-2"
       >
-        Sign In
-      </Button>
+        <Image
+          src="/svg/defaultProfile.svg"
+          width="36"
+          height="36"
+          alt="Logout"
+          className="rounded-full"
+        />
+      </button>
     </form>
   );
 }
