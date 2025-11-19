704584e7cd63a4e0f2999939da91b7cd68523d1c
gpalmer27 <palmer.gi@northeastern.edu>
2025-11-18 22:59:03 -0500

    updated review card designs

--- apps/web/src/app/_components/reviews/review-card.tsx
+++ apps/web/src/app/_components/reviews/review-card.tsx
@@ -7,13 +7,9 @@ import { cn } from "@cooper/ui";
 import { Card, CardContent } from "@cooper/ui/card";
 
 import { api } from "~/trpc/react";
-import { locationName } from "~/utils/locationHelpers";
-import {
-  abbreviatedWorkTerm,
-  prettyWorkEnviornment,
-} from "~/utils/stringHelpers";
+import { prettyLocationName } from "~/utils/locationHelpers";
+import { prettyWorkEnviornment } from "~/utils/stringHelpers";
 import { DeleteReviewDialog } from "./delete-review-dialogue";
-import { ReviewCardStars } from "./review-card-stars";
 
 interface ReviewCardProps {
   className?: string;
@@ -27,58 +23,68 @@ export function ReviewCard({ reviewObj, className }: ReviewCardProps) {
   // Check if the current user is the author of the review
   const isAuthor = currentProfile?.id === reviewObj.profileId;
 
+  const { data: location } = api.location.getById.useQuery({
+    id: reviewObj.locationId ?? "",
+  });
+
   return (
     <Card
       className={cn(
-        "mx-auto min-h-40 w-[100%] border-[0.75px] border-cooper-gray-400 bg-cooper-gray-100",
+        "mx-auto w-[100%] border-[0.75px] border-cooper-gray-150 bg-[#FEFEFE]",
         className,
       )}
     >
       <div className="flex w-full flex-wrap">
-        <div className="h-40 w-full sm:w-[35%]">
+        <div className="w-full sm:w-[20%]">
           <CardContent className="flex h-full flex-col justify-between pr-0">
             <div>
-              <div className="pt-2">
-                <ReviewCardStars numStars={reviewObj.overallRating} />
+              <div className="flex flex-row gap-2">
+                <div className="text-4xl text-[#151515]">
+                  {reviewObj.overallRating.toFixed(1)}
+                </div>
+                <Image
+                  src="/svg/star.svg"
+                  alt="Star icon"
+                  width={28}
+                  height={28}
+                />
               </div>
-              <div className="align-center flex flex-wrap gap-x-2 pt-2">
+              <div className="align-center flex flex-col  pt-2 text-cooper-gray-350">
                 <span
-                  className={`${locationName(reviewObj) ? "visibility: visible" : "visibility: hidden"}`}
+                  className={`${location && prettyLocationName(location) ? "visibility: visible" : "visibility: hidden"}`}
                 >
-                  {locationName(reviewObj)} •
+                  {prettyLocationName(location)}
                 </span>
                 <span>
-                  {abbreviatedWorkTerm(reviewObj.workTerm)} {reviewObj.workYear}
+                  {reviewObj.workTerm.charAt(0).toUpperCase() +
+                    reviewObj.workTerm.slice(1).toLowerCase()}{" "}
+                  {reviewObj.workYear}
                 </span>
-              </div>
-            </div>
-            <div className="flex flex-col">
-              <div className="text-sm">
-                {reviewObj.createdAt.toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}
               </div>
             </div>
           </CardContent>
         </div>
-        <div className="w-full sm:w-[65%]">
+        <div className="w-full sm:w-[80%]">
           <CardContent className="flex h-full flex-col justify-between gap-4 pl-4 sm:pl-0">
             <div className="flex flex-row justify-between">
               <div className="pt-1">{reviewObj.textReview}</div>
               {isAuthor && <DeleteReviewDialog reviewId={reviewObj.id} />}
             </div>
             <div className="flex justify-between text-sm">
-              <div className="flex gap-6">
-                <div>Position type: Co-op</div>
-                <div>
-                  Work model:{" "}
+              <div className="flex gap-10 bg-cooper-gray-100 p-3 pl-4 pr-4 rounded-lg">
+                <div className="flex flex-row gap-2">
+                  <span className="text-cooper-gray-350">Job type</span> Co-op
+                </div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-cooper-gray-350">Work model</span>
                   {prettyWorkEnviornment(
                     reviewObj.workEnvironment as WorkEnvironmentType,
                   )}
                 </div>
-                <div>Pay: ${reviewObj.hourlyPay} / hr</div>
+                <div className="flex flex-row gap-2">
+                  <span className="text-cooper-gray-350">Pay</span> $
+                  {reviewObj.hourlyPay} / hr
+                </div>
               </div>
 
               <Image
