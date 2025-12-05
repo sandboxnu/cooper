dd2880b4a3f7364f2769dfd65ac56c8eeb2838c4
tracyyh <huang.tr@northeastern.edu>
2025-12-05 14:59:07 -0500

    mobile responsiveness

--- apps/web/src/app/_components/filters/dropdown-filters-bar.tsx
+++ apps/web/src/app/_components/filters/dropdown-filters-bar.tsx
@@ -87,7 +87,7 @@ export default function DropdownFiltersBar({
       ];
 
   return (
-    <div className="flex flex-wrap gap-2">
+    <div className="flex gap-2">
       <DropdownFilter
         title="Industry"
         filterType="autocomplete"
