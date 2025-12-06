61629a61921fa46bc9ae1d60093000208f7ea969
gpalmer27 <palmer.gi@northeastern.edu>
2025-12-06 11:03:12 -0500

    migration

--- packages/db/drizzle/0005_productive_hulk.sql
+++ packages/db/drizzle/0005_productive_hulk.sql
@@ -1,4 +1,4 @@
 ALTER TABLE "profile" ALTER COLUMN "updatedAt" SET DEFAULT now();--> statement-breakpoint
 ALTER TABLE "profile" ALTER COLUMN "updatedAt" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "review" ADD COLUMN IF NOT EXISTS "snackBar" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "role" ADD COLUMN "jobType" varchar DEFAULT 'CO-OP';
+ALTER TABLE "role" ADD COLUMN IF NOT EXISTS "jobType" varchar DEFAULT 'CO-OP';
