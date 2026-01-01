1bcede8f41ff4f634b7db7c0d350ac76e0750129
gpalmer27 <palmer.gi@northeastern.edu>
2026-01-01 14:25:36 -0500

    misc changes

--- apps/web/src/app/_components/form/form-section.tsx
+++ apps/web/src/app/_components/form/form-section.tsx
@@ -1,8 +1,7 @@
-import { FormCard } from "~/app/_components/form/form-card";
 
 /**
  * FormSection component creates a section within a FormCard with a specified title.
  */
 export function FormSection({ children }: { children: React.ReactNode }) {
-  return <FormCard>{children}</FormCard>;
+  return <div className="flex flex-col rounded-lg w-full">{children}</div>;
 }
