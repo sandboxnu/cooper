7b91b34c25dc4a01bc192672091371e86923315a
gpalmer27 <palmer.gi@northeastern.edu>
2026-01-12 18:47:04 -0500

    fix lint

--- apps/web/src/app/_components/search/search-filter.tsx
+++ apps/web/src/app/_components/search/search-filter.tsx
@@ -6,7 +6,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-import type { IndustryType, LocationType } from "@cooper/db/schema";
 import { cn } from "@cooper/ui";
 import { Form } from "@cooper/ui/form";
 
