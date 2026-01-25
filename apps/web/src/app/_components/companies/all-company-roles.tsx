36522fb3397a6a04facf91dc7e95d332655b5019
Tiffany Uong <uong.t@northeastern.edu>
2026-01-25 16:04:19 -0500

    changed url to route to role

--- apps/web/src/app/_components/companies/all-company-roles.tsx
+++ apps/web/src/app/_components/companies/all-company-roles.tsx
@@ -34,7 +34,7 @@ export default function RenderAllRoles({ company }: RenderAllRolesProps) {
                   <div
                     key={role.id}
                     className="p-2"
-                    onClick={() => router.push(`/role?id=${role.id}`)}
+                    onClick={() => router.push(`/?company=${company?.name.toLowerCase()}&role=${role.title.toLowerCase()}`)}
                   >
                     <RoleCardPreview
                       roleObj={role}
