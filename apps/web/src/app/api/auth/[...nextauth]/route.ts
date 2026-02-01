a9b60f4bd88f79fc3607c6695c72038b02d5a706
gpalmer27 <palmer.gi@northeastern.edu>
2026-02-01 10:23:48 -0500

    changed route

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -40,13 +40,12 @@ export const GET = async (
 
   const nextauthAction = props.params.nextauth[0];
   const isExpoSignIn = req.nextUrl.searchParams.get("expo-redirect");
-  const cookieStore = await cookies();
-  const isExpoCallback = cookieStore.get(EXPO_COOKIE_NAME);
+  const isExpoCallback = cookies().get(EXPO_COOKIE_NAME);
 
   if (nextauthAction === "signin" && !!isExpoSignIn) {
     // set a cookie we can read in the callback
     // to know to send the user back to expo
-    cookieStore.set({
+    cookies().set({
       name: EXPO_COOKIE_NAME,
       value: isExpoSignIn,
       maxAge: 60 * 10, // 10 min
@@ -55,7 +54,7 @@ export const GET = async (
   }
 
   if (nextauthAction === "callback" && !!isExpoCallback) {
-    cookieStore.delete(EXPO_COOKIE_NAME);
+    cookies().delete(EXPO_COOKIE_NAME);
 
     // Run original handler, then extract the session token from the response
     // Send it back via a query param in the Expo deep link. The Expo app
