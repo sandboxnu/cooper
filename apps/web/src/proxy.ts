b5ff10b764bfc6683d5b777bc71fe2694f681508
gpalmer27 <palmer.gi@northeastern.edu>
2026-02-05 23:14:20 -0500

    added proxy

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,4 +1,4 @@
-export { auth as middleware } from "@cooper/auth";
+export { auth as proxy } from "@cooper/auth";
 
 // Or like this if you need to do something here.
 // export default auth((req) => {
