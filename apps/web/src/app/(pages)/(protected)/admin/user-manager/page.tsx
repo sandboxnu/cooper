358ff83d0382559cacf708f20a0d64664907c1fa
gpalmer27 <palmer.gi@northeastern.edu>
2026-03-20 16:03:07 -0400

    added user manager table

--- apps/web/src/app/(pages)/(protected)/admin/user-manager/page.tsx
+++ apps/web/src/app/(pages)/(protected)/admin/user-manager/page.tsx
@@ -1,7 +1,9 @@
+import { AdminUserManagerTable } from "../../../../_components/admin/user-manager-table";
+
 export default function AdminUserManagerPage() {
   return (
-    <div className="flex h-full w-full items-center justify-center flex-col">
-      User Manager yay
+    <div className="flex h-full w-full flex-col px-6 py-4">
+      <AdminUserManagerTable />
     </div>
   );
 }
