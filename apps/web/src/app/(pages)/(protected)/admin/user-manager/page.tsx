a3112e330f5c1e5fa01c9cdc82cb89e1d964eb17
gpalmer27 <palmer.gi@northeastern.edu>
2026-03-20 16:06:30 -0400

    added new user box

--- apps/web/src/app/(pages)/(protected)/admin/user-manager/page.tsx
+++ apps/web/src/app/(pages)/(protected)/admin/user-manager/page.tsx
@@ -1,8 +1,10 @@
+import { CreateUserForm } from "~/app/_components/admin/create-user-form";
 import { AdminUserManagerTable } from "../../../../_components/admin/user-manager-table";
 
 export default function AdminUserManagerPage() {
   return (
     <div className="flex h-full w-full flex-col px-6 py-4">
+      <CreateUserForm />
       <AdminUserManagerTable />
     </div>
   );
