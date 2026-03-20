919dee5eb8440cbe22e34d49ab8aec8b07de0baf
Gillian Palmer <147459191+gpalmer27@users.noreply.github.com>
2026-03-19 20:08:29 -0400

    first draft of admin table (#398)

--- apps/web/src/app/(pages)/(protected)/admin/dashboard/page.tsx
+++ apps/web/src/app/(pages)/(protected)/admin/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import { AdminDashboardTable } from "../../../../_components/admin/dashboard-table";
+
 export default function AdminDashboardPage() {
   return (
-    <div className="flex h-full w-full items-center justify-center flex-col">
-      Dashboard yay
+    <div className="flex h-full w-full flex-col px-6 py-4">
+      <AdminDashboardTable />
     </div>
   );
 }
