fccc7d62b205ee71d92c3b278315b1ba09a09d9e
gpalmer27 <palmer.gi@northeastern.edu>
2026-03-29 15:29:12 -0400

    fixed db file

--- packages/db/drizzle/0017_wakeful_red_shift.sql
+++ packages/db/drizzle/0017_wakeful_red_shift.sql
@@ -6,13 +6,13 @@ ALTER TABLE "hidden" ADD COLUMN IF NOT EXISTS "deactivatedByAdminId" uuid;--> st
 ALTER TABLE "review" ADD COLUMN IF NOT EXISTS "hidden" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE "role" ADD COLUMN IF NOT EXISTS "hidden" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "flagged" ADD CONSTRAINT IF NOT EXISTS "flagged_deactivatedByAdminId_user_id_fk" FOREIGN KEY ("deactivatedByAdminId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "flagged" ADD CONSTRAINT "flagged_deactivatedByAdminId_user_id_fk" FOREIGN KEY ("deactivatedByAdminId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "hidden" ADD CONSTRAINT IF NOT EXISTS "hidden_deactivatedByAdminId_user_id_fk" FOREIGN KEY ("deactivatedByAdminId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "hidden" ADD CONSTRAINT "hidden_deactivatedByAdminId_user_id_fk" FOREIGN KEY ("deactivatedByAdminId") REFERENCES "public"."user"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
