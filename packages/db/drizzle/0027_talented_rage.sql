0e0d27f76b3af18083ae047af4b153c6217c149b
Michael Song <song.mich@northeastern.edu>
2026-05-09 16:34:45 -0400

    migration fixes hopefully

--- packages/db/drizzle/0027_talented_rage.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "profile" ALTER COLUMN "major" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "profile" ALTER COLUMN "graduationYear" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "profile" ALTER COLUMN "graduationMonth" DROP NOT NULL;
